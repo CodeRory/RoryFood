b5592abeaf9a2b5d68aabd02044cf5e2f52cb3cc
CodeRory <coderory@gmail.com>
2020-07-01 11:09:22 +0200

    My account

--- src/Profile.js
+++ src/Profile.js
@@ -1,20 +1,70 @@
 import React, { Component } from 'react';
-import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, Image, FlatList } from 'react-native';
+import Constants from 'expo-constants';
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import { AntDesign } from '@expo/vector-icons';
+
+
 
 export default class Profile extends Component {
-
-  constructor(props) {
-     super(props);
-     this.state = {
-       data:"",
-     };
-  }
-
   render() {
     return (
-      <View style={{flex:1,alignItems: 'center', justifyContent: 'center'}}>
-         <Text>Profile</Text>
+      <View style={styles.firstContainer}>
+        <View style={styles.header}>
+          <Image source={require('../assets/avatar.jpg')} style={styles.avatar}/>
+          <View style={styles.textContainer}>
+            <Text style={styles.title} >Monica Berry</Text>
+            <Text style={styles.subTitle} >monicaberry@gmail.com</Text>
+          </View>
+        <View>
+          <View>
+            <MaterialCommunityIcons name="android-messages" size={24} color="black" />
+            <Text>Messages</Text>
+          </View>
+          <View>
+            <MaterialCommunityIcons name="routes" size={24} color="black" />
+            <Text>My routes</Text>
+          </View>
+          <View>
+            <AntDesign name="logout" size={24} color="black" />
+            <Text>Log Out</Text>
+          </View>
+          
+        
+        
+        </View>        
+        </View>
       </View>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  avatar: {
+    borderRadius: 40,
+    height: 80,
+    width: 80,
+  },
+  firstContainer: {
+    flex: 1,
+
+  },
+  header: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginTop: 80,
+    marginLeft: 30,
+
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+
+  },
+  textContainer: {
+    marginLeft: 15,
+  }
+
+
+})
